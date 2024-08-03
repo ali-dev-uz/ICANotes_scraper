797579dad7c4b517e58652d969adede8fdfa0c99
Ali <sevararuzieva2023@gmail.com>
2024-08-03 13:30:31 +0800

    update

---
+++ config.py
@@ -0,0 +1,11 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASS")
+DB_NAME = env.str("DB_NAME")
+DB_HOST = env.str("DB_HOST")
+
