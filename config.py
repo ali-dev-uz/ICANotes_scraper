d43f887fe4e5de12d7ea3bbe2a26b1a795228bed
Ali <sevararuzieva2023@gmail.com>
2024-08-03 13:36:12 +0800

    update

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ from environs import Env
 env = Env()
 env.read_env()
 
-
+WEBSITE_LOGIN = env.str("WEBSITE_LOGIN")
+WEBSITE_PASSWORD = env.str("WEBSITE_PASSWORD")
 DB_USER = env.str("DB_USER")
 DB_PASS = env.str("DB_PASS")
 DB_NAME = env.str("DB_NAME")
 DB_HOST = env.str("DB_HOST")
+CHROMEDRIVER_PATH = env.str("CHROMEDRIVER_PATH")
+
 
